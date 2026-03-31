5a213f711c6d71e11c1de89876006a13f5c94581
Vladu Fabian <vladu.fabian1@gmail.com>
2026-04-01 01:30:52 +0300

    created

---
+++ Seminar4_Singleton/src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+
+import clase.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger instanta1 = Logger.getInstanta("Interfata", "afisare");
+        instanta1.afiseazaLog("fisierul JSON nu a afisat corect");
+        instanta1.afiseazaLog("fisierul XML nu este formatat corespunzator");
+
+        Logger instanta2 = Logger.getInstanta("Backend", "baza de date");
+        instanta2.afiseazaLog("eroare la conexiunea cu BD");
+
+        instanta2.setSender("Backend");
+        instanta2.setCategorie("baza de date");
+        instanta2.afiseazaLog("eroare la inserare");
+
+    }
+}
